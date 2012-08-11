f225b011da0fca51b951bedd7188c22cc1c3c842
john <john@hesse>
2012-08-11 13:39:45 +0200

    externalize config, prepare authentication

--- src/transmission-wrapper-ui.py
+++ src/transmission-wrapper-ui.py
@@ -1,7 +1,17 @@
 #!env python
+"""
+Configuration file format:
+(order of lines matters)
+--
+user@host # to connect on remote host
+username/password #for web access
+--
 
+"""
 import web
 import subprocess
+
+TRANS = 'transmission-remote --auth=transmission:transmission'
 
 urls = (
     '/(.*)', 'executor',
@@ -52,17 +62,12 @@ Delete
 
 """
 
-
-app = web.application (urls, globals())
-
-HOST = 'ssh torrents@svpnproxy'
-TRANS = 'transmission-remote --auth=transmission:transmission'
-
 def render (status):
     return TEMPLATE % {'list': status}
 
 def transmission (cmd):
-    cmd = '%s %s %s' % (HOST, TRANS, cmd)
+    cmd = '%s %s %s' % (web.ctx.HOST, web.ctx.TRANS, cmd)
+    print cmd
     return subprocess.check_output (cmd, shell=True)
 
 _ = lambda x: render (transmission(x))
@@ -83,7 +88,7 @@ def remove():
 def start():
     url = web.input(url=None)['idx']
     _ ('-t %s -s' % url)
-    return status ()
+    raise web.seeother('/')
 
 def stop():
     url = web.input(url=None)['idx']
@@ -101,6 +106,7 @@ CMDS = {
 
 class executor:
     def GET (self, cmd):
+        print
         if cmd is None or cmd == '':
             cmd = 'status'
 
@@ -109,5 +115,37 @@ class executor:
 
         return CMDS [cmd] ()
 
+def gen_set_globals ():
+    """web.py is braindead^H^H^H^H^H^H so simple that makes it retardedly complex to pass simple local-file defined global variables to handlers. way to go for a web framework that has as greatest asset its simplicity, folks."""
+    def g (handler):
+        web.ctx.HOST = HOST
+        web.ctx.TRANS = TRANS
+        return handler ()
+
+    return g
+
 if __name__ == '__main__':
+
+    global HOST
+
+    import sys
+
+    if len (sys.argv) != 3:
+        sys.stderr.write ('usage: APP port config')
+        sys.exit (-1)
+
+    conn = None
+    cred = ()
+
+    with (open (sys.argv [2])) as f:
+        l = f.readlines()
+        conn = l[0].strip()
+        cred = tuple ([x.strip() for x in l[1].split ('/')])
+
+    HOST = 'ssh %s' % conn
+
+    web.ctx.HOST = HOST
+    app = web.application (urls, globals())
+    app.add_processor (gen_set_globals())
+
     app.run()
--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
-nohup python transmission-wrapper-ui.py > transmission-wrapper-ui.log  &
+# params: [list port], [path to config file]
+
+nohup python transmission-wrapper-ui.py $* > transmission-wrapper-ui.log  &
 echo $! > transmission-wrapper-ui.pid
